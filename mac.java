ba507e8ce15696204fd0bc8934222de70aae0d36
Dmytro175 <61700142+Dmytro175@users.noreply.github.com>
2020-03-14 16:14:41 +0100

    Delete mac.java

--- mac.java
+++
@@ -1,8 +0,0 @@
-public class mac {
-    public static void main(String[] arg) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        for (int a : arr) {
-            System.out.println(a);
-        }
-    }
-}
