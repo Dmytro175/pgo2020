13577f42b86c6ac056f325a81c62845497fd1611
Dmytro175 <61700142+Dmytro175@users.noreply.github.com>
2020-03-14 16:14:25 +0100

    Add files via upload

---
+++ mac.java
@@ -0,0 +1,8 @@
+public class mac {
+    public static void main(String[] arg) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
