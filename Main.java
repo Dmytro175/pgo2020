fd842eebef993552cec7050ccb59592941437746
Dmytro175 <61700142+Dmytro175@users.noreply.github.com>
2020-03-16 17:14:25 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
     System.out.println(phone2.sendsMag(" Fajnie",phone1.numer));
     */
 
-    Car k1=new Car("lamborgini","zólty",254);
-    Car k2=new Car(" Ferari "," czerwony ",244);
+    Car k1=new Car("lamborgini","zólty",1);
+    Car k2=new Car(" Ferari "," czerwony ",3);
     System.out.println(k1.infaCar(" Zajebisty",k2.marka,k2.kolorz,k2.pojemnościsilnika));
     System.out.println(k2.infaCar(" Fajny",k1.marka,k1.kolorz,k1.pojemnościsilnika));
     }
