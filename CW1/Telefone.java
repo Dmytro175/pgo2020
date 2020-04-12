6a21b1b7a24a2455646bfed5bdd0bcfcde7795e6
Dmytro175 <61700142+Dmytro175@users.noreply.github.com>
2020-04-12 18:41:47 +0200

    Add files via upload

---
+++ CW1/Telefone.java
@@ -0,0 +1,13 @@
+package CW1;
+public class Telefone {    String marka;
+    double rozdzielczosc;
+    int numer;
+    public Telefone(String marka, double rozdzielczosc, int numer){
+        this.marka=marka;
+        this.rozdzielczosc=rozdzielczosc;
+        this.numer=numer;
+    }
+    public String sendsMag(String mag,int numer){
+        return "Wysylam wiadomosc"+ mag+" numer "+this.numer;
+    }
+}
