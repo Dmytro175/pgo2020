f39c12d39ecf43bcf72d44445614a88399e884a7
Dmytro175 <61700142+Dmytro175@users.noreply.github.com>
2020-04-12 18:53:16 +0200

    Add files via upload

---
+++ CW2/Airplane.java
@@ -0,0 +1,12 @@
+package CW2;
+public class Airplane {String marka;
+    int ilość_miejsc;
+    public Airplane(String marka,int ilość_miejsc){
+        this.marka=marka;
+        this.ilość_miejsc=ilość_miejsc;
+    }
+    public String infa1(String marka ,int ilość_miejsc ){
+        return "Infa"+" marka "+this.marka+" ilość_miejsc "+this.ilość_miejsc;
+    }
+
+}
---
+++ CW2/Main.java
@@ -0,0 +1,11 @@
+package CW2;
+public class Main {
+    public static void main(String[] args) {
+
+        Airplane Samolot1 =new Airplane("Boing 737",103);
+        Engine samolot2=new Engine(22000,3000);
+        System.out.println(Samolot1.infa1("Boing 737",103));
+        System.out.println(samolot2.infa2(22000,3000));
+        System.out.println(samolot2.czy());
+    }
+}
